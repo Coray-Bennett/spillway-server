0e9fe860420997938a66280b0ee3be7525083a81
Coray-Bennett <cob3218@rit.edu>
2024-07-24 00:50:08 -0400

    init spring boot application

---
+++ spillway/src/test/java/com/coraybennett/spillway/SpillwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coraybennett.spillway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpillwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
