0dde794f374db4537cbed81df56322768e6f9d77
Coray-Bennett <cob3218@rit.edu>
2024-07-25 01:13:10 -0400

    basic streaming endpoints functional (naive storage solution)
    Former-commit-id: f3117b0ef7c4fd8a0a717aebab2deb5d24c81ce1

--- spillway/src/main/java/com/coraybennett/spillway/controller/VideoController.java
+++ spillway/src/main/java/com/coraybennett/spillway/controller/VideoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class VideoController {
     final private String CONTENT_PATH_PREFIX = "src/main/java/com/coraybennett/spillway/content/";
 
-    @RequestMapping("/{tag}.m3u8")
+    @GetMapping("/{tag}.m3u8")
     public ResponseEntity<ByteArrayResource> getVideo(@PathVariable String tag) throws IOException {
         String playlistFile = tag + ".m3u8";
 
@@ -27,17 +28,45 @@ public class VideoController {
         headers.set("Content-Disposition", "attachment;filename=" + playlistFile);
 
         try {
-            File file = new File(CONTENT_PATH_PREFIX + String.format("%s/%s", tag, playlistFile));
-            if(!file.exists()) {
+            ByteArrayResource playlist = fileToByteArrayResource(CONTENT_PATH_PREFIX + String.format("%s/%s", tag, playlistFile));
+            if(playlist == null) {
                 return new ResponseEntity<>(new ByteArrayResource(new byte[]{}), HttpStatus.NOT_FOUND);
             }
 
-            byte[] bytes = Files.readAllBytes(file.toPath());
-            ByteArrayResource resource = new ByteArrayResource(bytes);
-
-            return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+            return new ResponseEntity<>(playlist, headers, HttpStatus.OK);
         } catch (IOException e) {
             return new ResponseEntity<>(new ByteArrayResource(new byte[]{}), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/{tag}/{filename}")
+    public ResponseEntity<ByteArrayResource> ts(
+        @PathVariable String tag, 
+        @PathVariable String filename
+    ) throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/vnd.apple.mpegurl");
+        headers.set("Content-Disposition", "attachment;filename=" + filename);
+        
+        try {
+            ByteArrayResource tsVideo = fileToByteArrayResource(CONTENT_PATH_PREFIX + String.format("%s/%s", tag, filename));
+            if(tsVideo == null) {
+                return new ResponseEntity<>(new ByteArrayResource(new byte[]{}), HttpStatus.NOT_FOUND);
+            }
+
+            return new ResponseEntity<>(tsVideo, headers, HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity<>(new ByteArrayResource(new byte[]{}), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private ByteArrayResource fileToByteArrayResource(String path) throws IOException {
+        File file = new File(path);
+        if(!file.exists()) {
+            return null;
+        }
+
+        byte[] bytes = Files.readAllBytes(file.toPath());
+        return new ByteArrayResource(bytes);
     }
 }
