1466d2f6c9587117d2e6d56ecf80a23855523095
Coray-Bennett <cob3218@rit.edu>
2024-07-27 03:49:13 -0400

    process playlist file after upload

--- spillway/src/main/java/com/coraybennett/spillway/service/VideoConversionService.java
+++ spillway/src/main/java/com/coraybennett/spillway/service/VideoConversionService.java
@@ -1,10 +1,15 @@
 package com.coraybennett.spillway.service;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.UUID;
 
 import org.slf4j.Logger;
@@ -17,18 +22,21 @@ import com.coraybennett.spillway.exception.VideoConversionException;
 @Service
 public class VideoConversionService {
     Logger logger = LoggerFactory.getLogger(VideoConversionService.class);
-    private final String outputDirectory = "content/";
+    private final String OUTPUT_DIRECTORY = "content/";
+    private final String VIDEO_ROUTE_PREFIX = "http://127.0.0.1:8081/video/";
 
     public String convertToHls(MultipartFile videoFile) throws VideoConversionException {
         try {
             // Generate a UUID for unique file names
             String uuid = UUID.randomUUID().toString();
-            Path outputPath = Paths.get(outputDirectory, uuid);
+            Path outputPath = Paths.get(OUTPUT_DIRECTORY, uuid);
             Files.createDirectory(outputPath);
 
             // Original file path
             Path inputPath = Paths.get(outputPath.toString(), videoFile.getOriginalFilename());
             Files.copy(videoFile.getInputStream(), inputPath, StandardCopyOption.REPLACE_EXISTING);
+
+            String outputPlaylist = outputPath.toAbsolutePath().toString() + "/" + uuid + ".m3u8";
 
             // Convert the video to HLS format using ffmpeg
             String ffmpegCommand = String.format(
@@ -42,7 +50,7 @@ public class VideoConversionService {
                 -f hls %s
                 """,
                 inputPath.toAbsolutePath().toString(), 
-                outputPath.toAbsolutePath().toString() + "/" + uuid + ".m3u8"
+                outputPlaylist
             );
             
             Process process = Runtime.getRuntime().exec(ffmpegCommand);
@@ -53,6 +61,26 @@ public class VideoConversionService {
             if (exitCode != 0) {
                 Files.delete(outputPath);
                 throw new VideoConversionException("FFmpeg conversion failed with exit code: " + exitCode);
+            }
+
+            File playlist = new File(outputPlaylist);
+            List<String> processedPlaylist;
+            try (Scanner scanner = new Scanner(playlist)) {
+                processedPlaylist = new ArrayList<>();
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    if (line.matches(uuid + "[0-9]+.ts")) {
+                        processedPlaylist.add(VIDEO_ROUTE_PREFIX + uuid + "/" + line);
+                        continue;
+                    }
+                    processedPlaylist.add(line);
+                }
+            }
+            
+            try (FileWriter writer = new FileWriter(playlist)) {
+                for (String line : processedPlaylist) {
+                    writer.write(line + "\n");
+                }
             }
 
             return uuid;
