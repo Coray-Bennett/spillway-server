525c27e502968b27e8c2146431e7d457afc258e6
Coray-Bennett <cob3218@rit.edu>
2024-07-27 03:07:48 -0400

    fix video upload

--- spillway/src/main/java/com/coraybennett/spillway/controller/FileUploadController.java
+++ spillway/src/main/java/com/coraybennett/spillway/controller/FileUploadController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +14,7 @@ import com.coraybennett.spillway.exception.VideoConversionException;
 import com.coraybennett.spillway.service.VideoConversionService;
 
 @RestController
+@RequestMapping("/upload")
 public class FileUploadController {
     private final VideoConversionService videoConversionService;
 
@@ -21,12 +23,12 @@ public class FileUploadController {
         this.videoConversionService = videoConversionService;
     }
 
-    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/single", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> uploadVideo(@RequestParam("file") MultipartFile videoFile) {
         try {
             // Call the service to convert the video file
-            videoConversionService.convertToHls(videoFile);
-            return ResponseEntity.ok("Video file uploaded and converted successfully!");
+            String uuid = videoConversionService.convertToHls(videoFile);
+            return ResponseEntity.ok(uuid);
         } catch (VideoConversionException e) {
             // Handle conversion failures
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error converting video file: " + e.getMessage());
