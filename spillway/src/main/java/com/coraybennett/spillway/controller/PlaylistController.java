bbed5c8127dea814c5706ce40f6ae55480e7e6ce
Coray-Bennett <cob3218@rit.edu>
2025-05-08 12:21:49 -0400

    adjust dockerfile, verify working with docker-compose

--- spillway/src/main/java/com/coraybennett/spillway/controller/PlaylistController.java
+++ spillway/src/main/java/com/coraybennett/spillway/controller/PlaylistController.java
@@ -1,16 +1,22 @@
 package com.coraybennett.spillway.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.coraybennett.spillway.model.Playlist;
 import com.coraybennett.spillway.model.Video;
 import com.coraybennett.spillway.repository.PlaylistRepository;
 import com.coraybennett.spillway.repository.VideoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/playlist")
--- spillway/src/main/java/com/coraybennett/spillway/repository/PlaylistRepository.java
+++ spillway/src/main/java/com/coraybennett/spillway/repository/PlaylistRepository.java
@@ -1,8 +1,9 @@
 package com.coraybennett.spillway.repository;
 
-import com.coraybennett.spillway.model.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.coraybennett.spillway.model.Playlist;
 
 @Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, String> {
