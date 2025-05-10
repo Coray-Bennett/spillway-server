bd27ab5837ca5023d8470167e265bab53a7344d1
Coray-Bennett <cob3218@rit.edu>
2025-05-09 20:35:00 -0400

    actuator b
    Former-commit-id: b5279e1ed97a43b3485cc9d20010244f9fc59bb5 [formerly d4c98535675cdca5eb140615ac08f1d73779761c]
    Former-commit-id: c9dcd77ca03466d2fed57aa1d96a3643f1ee1767
    Former-commit-id: bffe29233a6aec2453d4f196681490b9e71a81ad
    Former-commit-id: ca2fe8b108140994bba50a80ab3b8c2ca12cc83e

--- spillway/src/main/java/com/coraybennett/spillway/config/SecurityConfig.java
+++ spillway/src/main/java/com/coraybennett/spillway/config/SecurityConfig.java
@@ -40,7 +40,6 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(authz -> authz
                 .requestMatchers("/auth/**").permitAll()
-                .requestMatchers("/actuator/**").permitAll()
                 .requestMatchers(HttpMethod.GET).permitAll()
                 .requestMatchers(HttpMethod.POST).authenticated()
                 .anyRequest().permitAll()
