c3a6ca3ec810cbc415a66838425e7a3fdd65da6b
Coray-Bennett <cob3218@rit.edu>
2025-05-22 16:29:32 -0400

    unused import

--- spillway/src/main/java/com/coraybennett/spillway/config/SecurityConfig.java
+++ spillway/src/main/java/com/coraybennett/spillway/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.coraybennett.spillway.config;
 
 import com.coraybennett.spillway.security.JwtAuthenticationEntryPoint;
 import com.coraybennett.spillway.security.JwtRequestFilter;
-import com.coraybennett.spillway.service.api.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +12,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
